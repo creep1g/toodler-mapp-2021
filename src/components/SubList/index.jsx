3ce4e8593fc7e43eaf2055795ac76098cf213cb1
Toggi <thorgilshjalmarsson@gmail.com>
2021-11-24 22:44:35 +0000

    Tasks have been moved to a seperate view, longpress for both lists and tasks need to be fixed.

--- src/components/SubList/index.jsx
+++ src/components/SubList/index.jsx
@@ -1,33 +1,29 @@
 import React from 'react';
 import { View, FlatList, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
-import TaskList from '../TaskList';
 
 const SubList = function ({ lists, onLongPress, selectedLists, onSelect  }) {
 	
 
 	return (
-		<View>
+
+
 			<FlatList
 				numColumns={2}
 				data={lists}
 				renderItem={({ item, index} ) => (
-				<View style={[styles.list, { backgroundColor: item.color }]}>
+				<TouchableOpacity 
+					style={[styles.list, { backgroundColor: item.color }]}
+					onLongPress={() => onLongPress(item.id)}
+					onPress={() => onSelect(item.id)}
+				>
 					<View name="listName" style={ { flex: 1 } }>
-	
 						<Text style={styles.name}> { item.name } </Text>
-	
-						{/* <View name="taskList" styles={{ flex: 1, position: 'absolute', top: 0, left:0, width: 145 }}> */}
-							<TaskList ListId={item.id} />
-						{/* </View> */}
-
 					</View>
-				
-				</View>
+				</TouchableOpacity>
 				)}
 				keyExtractor={item => item.id}
 			/>
-		</View>
 	);
 };
 
---
+++ src/components/TaskList/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create ({
+	card: {
+		flex: 1,
+		backgroundColor: '#B3FCF3',
+		alignItems: 'center',
+		justifyContent: 'center',
+		alignContent: 'center',
+		margin: 5,
+		width: 250,
+		borderRadius: 20
+	},
+	heading: {
+		alignItems: 'center',
+		fontSize: 15,
+		fontWeight: 'bold',
+		margin: 10,
+		borderBottomColor: 'black',
+		borderBottomWidth: 2
+	},
+
+	description: {
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10,
+		marginBottom: 10,
+	}
+
+}); 
