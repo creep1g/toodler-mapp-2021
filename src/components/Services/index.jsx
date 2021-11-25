ff2aee614d03421eb0d2c468baaec1adeff75bdb
Toggi <thorgilshjalmarsson@gmail.com>
2021-11-25 23:37:17 +0000

    Finishing tasks is persistant, deleting as well

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -8,36 +8,103 @@ class DataDog {
 	finishedTasks;
 
 	constructor () {
-		boards = data.boards;
-		lists = data.lists;
-	}
-
-	getBoards (){
-		return boards;
+		this.boards = data.boards;
+		this.lists = data.lists;
+		this.tasks = data.tasks;
+		finishedTasks = [];
+		this.populateFinished();
 	}
 	
-	addBoard (board){
-		boards.push(board)
+	getBoards () {
+		return this.boards;
+	}
+	
+	addBoard (board) {
+		this.boards.push(board)
 	}
 
-	deleteBoard (id){
-		boards = boards.filter(function(value){
+	deleteBoard (id) {
+		this.boards = this.boards.filter(function(value){
 			return value.id !== id;
 		});
 	}
 	
-	getLists( callerId){
-		return lists.filter( (lists) => lists.boardId !== callerId);
+	getLists (callerId) {
+		return this.lists.filter(function(value){
+			return value.boardId === callerId;
+		});
 	}
 
 	deleteList (id) {
-		lists = lists.filter(function(value) {
+		this.lists = this.lists.filter(function(value) {
 			return value.id !== id;
 		});
 	}
 
 	addList (list) {
-		lists.push(list);
+		this.lists.push(list);
+	}
+
+	getTasks (callerId){
+		return this.tasks.filter(function(value){
+			return value.listId === callerId;
+		});
+	}
+
+	deleteTask (id) {
+		this.tasks = this.tasks.filter(function(value) {
+			return value.id !== id;
+		})
+	}
+
+	addTask (task) {
+		this.tasks.push(task);
+	}
+
+	markFinished (id) {
+		this.tasks.forEach(
+			function(task){
+				if (task.id === id){
+					if (task.isFinished === false){
+					task.isFinished = true;
+				
+					}
+					else{
+						task.isFinished = false;
+					}
+				}
+			}
+		)
+		if (finishedTasks.includes(id)){
+			console.log("now");
+			finishedTasks = finishedTasks.filter(function(value){ return value !== id })
+		}
+		else{
+			finishedTasks.push(id);
+		}
+
+			
+	}
+
+	populateFinished () {
+		this.tasks.forEach( 
+			function(task){
+				if (task.isFinished === true){
+					finishedTasks.push(task.id);
+				}
+			}
+		);
+	}
+	getFinishedTasks (callerId) {
+		let filter = [];
+		this.tasks.forEach(
+			function(task){
+				if (finishedTasks.includes(task.id) && task.listId === callerId){
+					filter.push(task.id);
+				}	
+			}
+		);
+		return filter; 
 	}
 
 }
